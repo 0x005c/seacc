8e53cda8243cc5877afbdc2c95c5857d8019c358
0x005c <yk@coins.tsukuba.ac.jp>
2019-12-09 13:17:59 +0900

    Add error_at() which emits more clear error message

--- seacc.c
+++ seacc.c
@@ -22,6 +22,21 @@ struct Token {
 
 Token *token;
 
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, "");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 void error(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -39,13 +54,13 @@ bool consume(char op) {
 
 void expect(char op) {
   if(token->kind != TK_RESERVED || token->str[0] != op)
-    error("not '%c'", op);
+    error_at(token->str, "not '%c'", op);
   token = token->next;
 }
 
 int expect_number() {
   if(token->kind != TK_NUM)
-    error("not a number");
+    error_at(token->str, "not a number");
   int val = token->val;
   token = token->next;
   return val;
@@ -97,6 +112,8 @@ int main(int argc, char *argv[]) {
     error("too many/few arguments");
     return 1;
   }
+
+  user_input = argv[1];
 
   token = tokenize(argv[1]);
 
