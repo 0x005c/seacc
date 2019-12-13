63a4b42e67d3a9cedf1ec67ef3d99ec1972305f7
0x005c <yk@coins.tsukuba.ac.jp>
2019-12-13 13:33:03 +0900

    Update tests

--- test.sh
+++ test.sh
@@ -16,23 +16,23 @@ try() {
   fi
 }
 
-try 0 0
-try 42 42
-try 21 '5+20-4'
-try 41 ' 12 + 34 - 5 '
-try 47 '5+6*7'
-try 15 '5*(9-6)'
-try 4 '(3+5)/2'
-try 3 '-10 + 13'
-try 5 '+5'
-try 3 '-(-3)'
-try 0 '-3>3'
-try 1 '50*2>=100'
-try 1 '-3<=3'
-try 1 '50/20<100'
-try 1 '100+23==123'
-try 0 '123==456'
-try 0 '123!=123'
-try 1 '123!=456'
+try 0 'return 0;'
+try 42 'return 42;'
+try 21 'return 5+20-4;'
+try 41 'return 12 + 34 - 5 ;'
+try 47 'return 5+6*7;'
+try 15 'return 5*(9-6);'
+try 4 'return (3+5)/2;'
+try 3 'return -10 + 13;'
+try 5 'return +5;'
+try 3 'return -(-3);'
+try 0 'return -3>3;'
+try 1 'return 50*2>=100;'
+try 1 'return -3<=3;'
+try 1 'return 50/20<100;'
+try 1 'return 100+23==123;'
+try 0 'return 123==456;'
+try 0 'return 123!=123;'
+try 1 'return 123!=456;'
 
 echo OK
