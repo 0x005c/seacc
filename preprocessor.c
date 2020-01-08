5696b88b83f3b22bd2cb2bec7b7b547723de81f2
0x005c <yk@coins.tsukuba.ac.jp>
2020-01-08 13:45:43 +0900

    Self compiling of preprocessor.c succeeded

--- preprocessor.c
+++ preprocessor.c
@@ -1,25 +1,27 @@
-#include <stdlib.h>
-#include <string.h>
-
 #include "seacc.h"
+
+int strncmp(char *l, char *r, long len);
+void *calloc(long nmemb, long size);
+char *strncpy(char *dest, char *src, long len);
 
 struct Token *preprocess() {
   if(consume("#")) {
     struct Token *directive = consume_ident();
-    if(!directive) error_at(token->next->str, "identifier expected");
+    if(directive == 0) error_at(token->next->str, "identifier expected");
     if(strncmp(directive->str, "include", 7))
       error_at(directive->str, "directives except include are not implemented");
     if(token->kind != TK_STRING_LITERAL)
       error_at(token->str, "string literal expected");
-    char *filename = calloc(1, token->len-1);
-    strncpy(filename, token->str+1, token->len-2);
+    char *fname = calloc(1, token->len-1);
+    strncpy(fname, token->str+1, token->len-2);
 
-    char *code = read_file(filename);
+    char *code = read_file(fname);
     struct Token *cur = tokenize(code);
     struct Token *head = cur;
 
     // TODO: check cur->next
-    for(; cur->next->next; cur=cur->next);
+    for(; cur->next->next; cur=cur->next) {
+    }
     // skip TK_EOF
     cur->next = token->next;
 
--- test/test_self_compile.sh
+++ test/test_self_compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./seacc 'main.c' > main.s
 ./seacc 'reader.c' > reader.s
-gcc -o seacc -fpack-struct=1 main.s reader.s error.c codegen.c parser.c preprocessor.c tokenizer.c
+./seacc 'preprocessor.c' > preprocessor.s
+gcc -o seacc -fpack-struct=1 main.s reader.s preprocessor.s error.c codegen.c parser.c tokenizer.c
