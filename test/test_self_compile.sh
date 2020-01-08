615f9901a56b86ae5dc8c294bb0726db0356592a
0x005c <yk@coins.tsukuba.ac.jp>
2020-01-08 13:33:47 +0900

    Self-compile of reader.c succeeded

--- test/test_self_compile.sh
+++ test/test_self_compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ./seacc 'main.c' > main.s
-gcc -o seacc -fpack-struct=1 main.s error.c codegen.c parser.c preprocessor.c reader.c tokenizer.c
+./seacc 'reader.c' > reader.s
+gcc -o seacc -fpack-struct=1 main.s reader.s error.c codegen.c parser.c preprocessor.c tokenizer.c
