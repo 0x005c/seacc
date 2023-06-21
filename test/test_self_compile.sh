05051f303b3309e3213cb857820ecd72325ddb29
0x005c <52650857+0x005c@users.noreply.github.com>
2023-06-22 00:23:56 +0900

    GCC 10 以降でも test/test_self_compile.sh が実行できるよう修正

--- test/test_self_compile.sh
+++ test/test_self_compile.sh
@@ -2,4 +2,5 @@
 ./seacc 'main.c' > main.s
 ./seacc 'reader.c' > reader.s
 ./seacc 'preprocessor.c' > preprocessor.s
-gcc -o seacc -fpack-struct=1 main.s reader.s preprocessor.s error.c codegen.c parser.c tokenizer.c
+gcc -o seacc -fcommon -fpack-struct=1 \
+  main.s reader.s preprocessor.s error.c codegen.c parser.c tokenizer.c
